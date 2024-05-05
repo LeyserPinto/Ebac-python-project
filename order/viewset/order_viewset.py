f26314c77e051cbddd50487b913f88caaceaf98e
Leyser Pinto <leyserpinto3@gmail.com>
2024-05-05 11:15:44 -0300

    :tada: add restapp github actions build

--- order/viewset/order_viewset.py
+++ order/viewset/order_viewset.py
@@ -1,4 +1,3 @@
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
 from rest_framework.mixins import CreateModelMixin
@@ -9,7 +8,5 @@ from order.models import Order
 from order.serializers import OrderSerializers
 
 class OrderViewSet(ModelViewSet):
-    authentication_classes = [SessionAuthentication, BasicAuthentication, TokenAuthentication]
-    permission_classes = [IsAuthenticated]
     serializer_class = OrderSerializers
     queryset         = Order.objects.all().order_by('id')
