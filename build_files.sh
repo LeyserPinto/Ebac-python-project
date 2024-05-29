4fae8945602a36736801a6e076b42f76d8b6ce5e
Leyser Pinto <59316466+LeyserPinto@users.noreply.github.com>
2024-05-28 22:44:02 -0300

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
-# pip3 install -r requirements.txt 
-# python3.9 manage.py collectstatic
+pip3 install -r requirements.txt 
+python3.11 manage.py collectstatic
 
-pip3 install poetry
-poetry install
-poetry run python manage.py collectstatic
+# pip3 install poetry
+# poetry install
+# poetry run python manage.py collectstatic
