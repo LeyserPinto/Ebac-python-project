4af56c9e48c9f8e76f5d7ef6638eb44f347904c8
Leyser Pinto <leyserpinto3@gmail.com>
2024-06-09 11:53:38 -0300

    Fix Python-Django Settings

--- restapp/settings.py
+++ restapp/settings.py
@@ -16,7 +16,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -24,11 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-f*k@=53bc5!shef1-6w+m$-g)kspbaljz%8k4(j7iuc-u2_dyd"
 # SECRET_KEY = os.environ.get("SECRET_KEY") 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = int(os.environ.get('DEBUG', default=0)) 
-DEBUG = False
+# DEBUG = False
+DEBUG = int(os.environ.get('DEBUG', default=0)) 
 
 # ALLOWED_HOSTS =  os.environ.get('DJANGO_ALLOWED_HOSTS', '').split(" ")
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.vercel.app', 'leyserp.pythonanywhere.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'leyserp.pythonanywhere.com']
 
 
 # Application definition
@@ -141,8 +140,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
