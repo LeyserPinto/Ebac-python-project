5e46ffaa3d6e1fc68f4c7d445f2a257b8bd53e5a
Leyser Pinto <leyserpinto3@gmail.com>
2024-06-09 12:51:14 -0300

    :tada: add app templates

--- restapp/settings.py
+++ restapp/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'restapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'restapp', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
